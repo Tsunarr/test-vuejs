bcbfec92da44787f0c3aff0f56676e60b945fba9
Tsuna77 <r77raphy@gmail.com>
2024-03-18 19:22:50 +0000

    test: fix vote config file format

--- vitest.config.mjs
+++ vitest.config.mjs
@@ -1,6 +1,6 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-const path = require('path')
+import path from 'path'
 
 
 // https://vitejs.dev/config/
@@ -12,7 +12,7 @@ export default defineConfig({
  },
   resolve: {
     alias: {
-      '@': path.resolve(__dirname, 'src/')
+      '@': path.resolve(process.cwd(), 'src/')
     }
   }
 })
